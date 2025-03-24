45f8305ac87c5362ff79a20d4fed9da76792e82c
Jomar Nobleza <jomarnobleza011@gmail.com>
2025-03-24 11:36:09 +0800

    Update function.js

--- Getting function/function.js
+++ Getting function/function.js
@@ -1,34 +1,34 @@
-// // // // var dogName = "Rover"
-// // //  // var dogWeight = 23;
-// // //  // if (dogWeight > 20){
-// // //  //     console.log(dogName + " says WOOF WOOF ")
-// // //  // }else{
-// // //  //     console.log(dogName + " says arf arf ")
-// // //  // }
+// var dogName = "Rover"
+ // var dogWeight = 23;
+ // if (dogWeight > 20){
+ //     console.log(dogName + " says WOOF WOOF ")
+ // }else{
+ //     console.log(dogName + " says arf arf ")
+ // }
  
-// // //  // dogName = "mark";
-// // //  // dogWeight = 13;
-// // //  // if (dogWeight >20){
-// // //  //     console.log(dogName + " says WOOF WOOF ")
-// // //  // } else {
-// // //  //     console.log (dogName + " says ARF ARF ")
-// // //  // }
+ // dogName = "mark";
+ // dogWeight = 13;
+ // if (dogWeight >20){
+ //     console.log(dogName + " says WOOF WOOF ")
+ // } else {
+ //     console.log (dogName + " says ARF ARF ")
+ // }
  
-// // //  // dogName = "jane";
-// // //  // dogWeight = 15;
-// // //  // if (dogWeight >20){
-// // //  //     console.log(dogName + " says WOOF WOOF ")
-// // //  // } else {
-// // //  //     console.log (dogName + " says ARF ARF ")
-// // //  // }
+ // dogName = "jane";
+ // dogWeight = 15;
+ // if (dogWeight >20){
+ //     console.log(dogName + " says WOOF WOOF ")
+ // } else {
+ //     console.log (dogName + " says ARF ARF ")
+ // }
  
-// // //  // dogName = "ana";
-// // //  // dogWeight = 19;
-// // //  // if (dogWeight >20){
-// // //  //     console.log(dogName + " says WOOF WOOF ")
-// // //  // } else {
-// // //  //     console.log (dogName + " says ARF arf arf ")
-// // //  // }
+ // dogName = "ana";
+ // dogWeight = 19;
+ // if (dogWeight >20){
+ //     console.log(dogName + " says WOOF WOOF ")
+ // } else {
+ //     console.log (dogName + " says ARF arf arf ")
+ // }
  
  function bark(name, weight){
     if(weight > 20){
