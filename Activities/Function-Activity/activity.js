fbb3efb87cda8eb0ae4733b7e95ea26369e5f5d0
Jomar Nobleza <jomarnobleza011@gmail.com>
2025-03-24 11:33:27 +0800

    jomar

---
+++ Activities/Function-Activity/activity.js
@@ -0,0 +1,15 @@
+function whatShallIWear(temp){
+    if (temp < 60){
+        console.log("Wear a jacket");
+    }
+else if (temp < 70){
+    console.log("Wear a sweater");
+}
+
+else {(temp < 70)
+    console.log("Wear a t-shirt");
+}
+}
+whatShallIWear(60);
+whatShallIWear(80);
+whatShallIWear(50);
