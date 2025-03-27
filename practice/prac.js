8bccc34bdb1b5d306556fdb6fcaa501a2aa32838
jomar11346 <jomarnobleza011@gmail.com>
2025-03-27 21:31:51 +0800

    practice

--- practice/prac.js
+++ practice/prac.js
@@ -1,8 +1,11 @@
 function bake(degree) {
     var message;
+
     if (degrees > 500) {
         message = "I'm not a refrigerator";
-    } else {
+    } 
+    else {
+
         message = "That's avery comfortable temperature for me";
         setMode("Bake");
         setTemp(degrees);
